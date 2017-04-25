69783e2fbb591a3cb2de9f474cc5d8fec5c0a21d
yoshhiide <nabeyama@outlook.com>
2017-04-26 00:00:35 +0900

    add overlap method

--- example.js
+++ example.js
@@ -3,6 +3,7 @@
 const timeRange = require('./lib/time-range');
 //const timeRange = require('time-range');
 
+// range calc
 const times = [
   [ new Date(2017, 3, 24, 10, 10, 0), new Date(2017, 3, 25, 2, 10, 30) ],
   [ new Date(2017, 3, 24, 20, 20, 0), new Date(2017, 3, 25, 2, 10, 30) ],
@@ -12,3 +13,15 @@ const times = [
 
 console.log( 'total minutes = ' + timeRange.calc(times) );
 console.log( 'total seconds = ' + timeRange.calc(times, 'seconds') );
+
+// overlap check
+const times2 = [
+  [ new Date(2017, 3, 24, 10, 10, 0), new Date(2017, 3, 25, 2, 10, 30) ],
+  [ new Date(2017, 3, 25, 2, 10, 29), new Date(2017, 3, 25, 2, 30, 30) ],
+];
+
+if (timeRange.overlap(times2)) {
+  console.log( 'not overlap.' );
+} else {
+  console.log( 'overlap or bad case.' );
+}
